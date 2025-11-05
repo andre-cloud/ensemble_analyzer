bafc589519ce133d229baab96112ea01ed0d1ed4
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2025-11-05 09:57:24 +0100

    ORCA version

--- src/graph.py
+++ src/graph.py
@@ -104,6 +104,7 @@ class Computed(Graph):
         self.g = convolution if convolution else self.graph_type
         self.conformers = conf
         idx_first_valid_conf = [idx for idx, i in enumerate(conf) if i.active][0]
+        
         if (
             conf[idx_first_valid_conf].energies[self.protocol].get("graph", None)
             is None
