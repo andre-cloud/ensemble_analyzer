a48d070970c46b331f70f652dad6741107641f58
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2025-11-26 20:52:53 +0100

    Update formatter.py

---
+++ src/logger/formatter.py
@@ -0,0 +1,18 @@
+
+import logging
+
+
+class ColoredFormatter(logging.Formatter):
+    COLORS = {
+        logging.DEBUG: "\033[90m",
+        logging.INFO: "\033[37m",
+        logging.WARNING: "\033[33m",
+        logging.ERROR: "\033[31m",
+        logging.CRITICAL: "\033[1;31m",
+    }
+    RESET = "\033[0m"
+
+    def format(self, record):
+        color = self.COLORS.get(record.levelno, self.RESET)
+        msg = super().format(record)
+        return f"{color}{msg}{self.RESET}"
