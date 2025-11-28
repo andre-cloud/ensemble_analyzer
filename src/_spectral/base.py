a216b06fc93c7c1240aa3d462e1780b91747387e
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2025-11-28 15:03:56 +0100

    Update base.py

--- src/_spectral/base.py
+++ src/_spectral/base.py
@@ -51,6 +51,8 @@ class BaseGraph:
             x = np.array(conf.graphs_data.__getitem__(protocol_number=protocol.number, graph_type=self.graph_type).X)
             y = np.array(conf.graphs_data.__getitem__(protocol_number=protocol.number, graph_type=self.graph_type).Y) * p
 
+            print(x)
+
             if x.size < 1:
                 continue
             if self.invert: 
