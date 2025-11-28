9eed4b6fb40c884614f09e20150cf1a32bf50d1b
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2025-11-28 12:43:38 +0100

    Update logger.py

--- src/_logger/logger.py
+++ src/_logger/logger.py
@@ -50,7 +50,7 @@ class Logger(logging.Logger):
         self.info('Protocol Steps:')
         for protocol in config.get('protocols', []):
             self.info(f"{int(protocol.number):03d}. {self.ARROW} {str(protocol)} {self.SPLIT} {protocol.calculation_level}" +
-                      f"{self.SPLIT} {protocol.comment}" if protocol.comment else "" +
+                      (f"{self.SPLIT} {protocol.comment}" if protocol.comment else "" ) + 
                       f"\n {protocol.thr}")            
         self._separator()
 
