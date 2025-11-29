8a46e3e49f79e306725f900383881c5fe6c27036
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2025-11-29 16:21:17 +0100

    Update protocol_executor.py

--- src/_managers/protocol_executor.py
+++ src/_managers/protocol_executor.py
@@ -159,9 +159,9 @@ class ProtocolExecutor:
         # If retention rate is lower than 30% and TODO: setting per disabilitarlo
         retention_rate = final_active / active_count if active_count > 0 else 1.0
 
-        if retention_rate < MIN_RETENTION_RATE:
+        if retention_rate < MIN_RETENTION_RATE and not (isinstance(protocol.cluster, int) and protocol.cluster > 1):
             self.logger.critical(
-                f'✗ Ensemble reduce more than {(1-retention_rate)*100:.1f}%. Calculation will stop.\n'
+                f'✗ Ensemble reduce by {(1-retention_rate)*100:.1f}%. Calculation will stop.\n'
                 f'\t{self.logger.WARNING} threshold: {MIN_RETENTION_RATE*100:.0f}%. Stopping.'
             )
             raise "Calculation ended for too much pruning"
