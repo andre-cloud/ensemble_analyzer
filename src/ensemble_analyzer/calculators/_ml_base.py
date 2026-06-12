6a51ddc0bd58db398fdd178469418b973c8af8e1
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2026-06-12 12:07:20 +0200

    Update _ml_base.py

--- src/ensemble_analyzer/calculators/_ml_base.py
+++ src/ensemble_analyzer/calculators/_ml_base.py
@@ -120,9 +120,13 @@ class BaseMlCalc(BaseCalc):
         atoms = self.conf.get_ase_atoms(calc)
         self._apply_ase_constraints(atoms)
         start = time.perf_counter()
-        Opt = Sella if self.protocol.ts else LBFGS
-        with Opt(atoms, maxstep=self.protocol.maxstep) as opt:
-            opt.run(fmax=self.protocol.fmax)
+
+        if self.protocol.ts:
+            with Sella(atoms) as opt:
+                opt.run(fmax=self.protocol.fmax)
+        else:
+            with LBFGS(atoms, maxstep=self.protocol.maxstep) as opt:
+                opt.run(fmax=self.protocol.fmax)
 
         self.conf.last_geometry = atoms.get_positions().copy()
         if self.protocol.freq:
