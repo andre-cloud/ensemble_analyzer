f29eda40fe7f24b6921493caa3d1e34732e806bd
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2026-07-01 16:59:55 +0200

    Update _skala.py

--- src/enan_calculators/_skala.py
+++ src/enan_calculators/_skala.py
@@ -21,7 +21,7 @@ _PREDICTOR_CACHE = {}
 
 def create_skala_calc(charge, mult, method, basis, solvent=None):
 
-    model_path = get_models_dir("", create=False) / method
+    model_path = get_models_dir("skala", create=False) / method
     if not model_path.exists():
         model_path_fun = Path(str(model_path) + ".fun")
         if model_path_fun.exists():
